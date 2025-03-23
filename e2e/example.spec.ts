03feefe3502bab518b3ab174099b88d9509bf8b2
Satyam Seth <satyam1998.1998@gmail.com>
2025-03-23 12:05:08 +0530

    Fix e2e test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,8 +1,8 @@
 import { expect, test } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://audio-player.satyamseth.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Audio Player/);
 });
